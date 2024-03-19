dc89a1f7a4aeb1d41e685d30de7ceb5afc8f3344
Shivani-Chouksey <122255940+Shivani-Chouksey@users.noreply.github.com>
2024-03-19 17:44:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 // app.use(cors());
-app.use(cors({origin:'http://localhost:3000'}));
+app.use(cors({origin:'*'}));
 
 
 const transporter = nodemailer.createTransport({
